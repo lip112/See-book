017829c4759c5166350bc7fc82e068aa8b93b293
mixmaster <whaud1597@naver.com>
2025-01-01 21:05:10 +0900

    @PostConstruct -> ApplicationRunner

--- src/main/java/com/example/seebook/domain/DataInitializer.java
+++ src/main/java/com/example/seebook/domain/DataInitializer.java
@@ -6,19 +6,25 @@ import com.example.seebook.domain.role.repository.RoleRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
-@RequiredArgsConstructor
-public class DataInitializer{
+public class DataInitializer implements ApplicationRunner {
 
     private final RoleRepository roleRepository;
 
-    @PostConstruct
-    public void run(){
+    @Autowired
+    public DataInitializer(RoleRepository roleRepository){
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         roleRepository.saveAll(Arrays.asList(
                 new RoleInfo(RoleCode.ADMIN),
                 new RoleInfo(RoleCode.USER)
